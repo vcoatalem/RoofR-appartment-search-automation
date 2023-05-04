776a8e525ba3ec2982a3376d8a8f57b0c64927f5
Victor Coatalem <victor.coatalem@epita.fr>
2023-05-04 19:31:38 +0200

    put annonce logic in its own file

---
+++ domain/domain_types.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import re
+
+
+@dataclass
+class Annonce:
+    def __init__(self, id: int, url: str) -> None:
+        self.id = id
+        self.url = url
+
+
+def ParseAnnonceFromUrl(url: str) -> Annonce:
+
+    def extract_annonce_id_from_url(url: str) -> int:
+        extract_id_pattern = r"/(\d+)\.htm"
+        match = re.search(pattern=extract_id_pattern, string=url)
+        if match:
+            number = int(match.group(1))
+            return number
+        return -1
+
+    res = Annonce(
+        id=extract_annonce_id_from_url(url),
+        url=url
+    )
+
+    if res.id == -1:
+        return None
+    
+    return res
