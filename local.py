79f9e00d718a2e16ea449ed7889e3fc14ab779f1
Victor Coatalem <victor.coatalem@epita.fr>
2023-05-08 18:28:05 +0200

    add new entry point for lambda calls using python-lambda

---
+++ local.py
@@ -0,0 +1,28 @@
+
+import itertools
+import os
+
+from src.domain.domain_types import Annonce, ContactInformation
+from src.adapters.inbox.gmail_adapter import GmailAdapter
+from src.adapters.cache.dynamodb_adapter import DynamodbAdapter
+from src.adapters.cache.csv_adapter import CSVAdapter
+from src.adapters.annonceApi.mock_adapter import MockAdapter
+from src.adapters.annonceApi.seloger_adapter import SeLogerAdapter
+
+
+from src.domain.domain import answer_all_annonces
+
+if __name__ == '__main__':
+    gmailAdapter = GmailAdapter.from_env()
+    contact = ContactInformation.from_env()
+    csvAdapter = DynamodbAdapter('find-a-roof')#CSVAdapter("annonces.csv")
+    api = MockAdapter()
+
+    csvAdapter.load()
+
+    print("annonce mails: ", answer_all_annonces(
+        inbox=gmailAdapter,
+        cache=csvAdapter,
+        api=api,
+        contact=contact
+    ))
--- service.py
+++ service.py
@@ -1,31 +1,27 @@
-
 import itertools
 import os
 
 from src.domain.domain_types import Annonce, ContactInformation
 from src.adapters.inbox.gmail_adapter import GmailAdapter
 from src.adapters.cache.dynamodb_adapter import DynamodbAdapter
-from src.adapters.cache.csv_adapter import CSVAdapter
-from src.adapters.annonceApi.mock_adapter import MockAdapter
 from src.adapters.annonceApi.seloger_adapter import SeLogerAdapter
-
 
 from src.domain.domain import answer_all_annonces
 
 def handler(event, context):
 
-    gmailAdapter = GmailAdapter.from_env()
+    inbox = GmailAdapter.from_env()
     contact = ContactInformation.from_env()
-    csvAdapter = DynamodbAdapter('find-a-roof')#CSVAdapter("annonces.csv")
-    api = MockAdapter()
+    cache = DynamodbAdapter('find-a-roof')#CSVAdapter("annonces.csv")
+    api = SeLogerAdapter()
 
-    csvAdapter.load()
+    cache.load()
 
     print("annonce mails: ", answer_all_annonces(
-        inbox=gmailAdapter,
-        cache=csvAdapter,
-        api=api,
-        contact=contact
+        inbox,
+        cache,
+        api,
+        contact
     ))
     return 0
 
