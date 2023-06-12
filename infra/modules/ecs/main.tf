a5178a232bde370e2aba47366b0011710042c3fa
Xarang <victor.coatalem@gmail.com>
2023-06-12 12:10:08 +0200

    infra, fix: assume role policies now working for ecs

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -31,21 +31,20 @@ data "aws_iam_policy" "task_execution_policy" {
   name = "AmazonECSTaskExecutionRolePolicy"
 }
 
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs-task-execution-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+  name = "ecs-${local.app_name}-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
