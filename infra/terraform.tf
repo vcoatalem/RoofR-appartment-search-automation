c639d9c5bd256bcf3c05c98267c776e71bd98188
Victor Coatalem <victor.coatalem@exalt-it.com>
2023-06-12 19:59:16 +0200

    infra: test terraform cloud

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -6,13 +6,22 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0.0"
     }
+    
   }
 
+  cloud {
+    organization = "victorcoatalem"
+
+    workspaces {
+      name = "example-workspace"
+    }
+  }
+  /*
   backend "s3" {
     bucket = "tfstate-far"
     dynamodb_table = "tfstate.lock"
     region = "eu-west-1"
     key    = "terraform.tfstate"
     encrypt = true
-  }
+  }*/
 }
