98e603b6267d4941fed5b014195e86e9079008a7
Victor Coatalem <victor.coatalem@exalt-it.com>
2023-06-12 19:26:59 +0200

    infra, clean : add state locking and s3 encryption

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -10,7 +10,9 @@ terraform {
 
   backend "s3" {
     bucket = "tfstate-far"
+    dynamodb_table = "tfstate.lock"
     region = "eu-west-1"
     key    = "terraform.tfstate"
+    encrypt = true
   }
 }
